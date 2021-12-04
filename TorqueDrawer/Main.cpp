db173464d5e802919d957fba1ce72ebe99e1fe31
Evicitter <evicitter@gmail.com>
2021-12-04 23:34:19 +0500

    init

---
+++ TorqueDrawer/Main.cpp
@@ -0,0 +1,76 @@
+﻿#include <string>
+#include <iostream>
+#include <fstream>
+
+//SFML
+#include <SFML\Graphics.hpp>
+
+//utils
+#include "utils\CurveEval.hpp"
+
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+int wmain()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), "");
+	window.setVerticalSyncEnabled(true);
+	ImGui::SFML::Init(window);
+
+	//float my_values[] = { 0.2f, 0.1f, 1.0f, 0.5f, 0.9f, 2.2f };
+
+	bool p_open = true;
+
+	bool bsel_torquedraw = true;
+	bool bsel_powerdraw = false;
+
+	//window.setTitle();
+	window.resetGLStates(); // call it if you only draw ImGui. Otherwise not needed.
+	sf::Clock deltaClock;
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			ImGui::SFML::ProcessEvent(event);
+
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		ImGui::SFML::Update(window, deltaClock.restart());
+
+		ImGui::SetNextWindowSize(ImVec2(500, 110));
+		ImGui::SetNextWindowPos(ImVec2(0,0));
+		ImGui::Begin("Options Layer", &p_open, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_MenuBar); // begin window
+
+		if (ImGui::BeginMenuBar())
+		{
+			if (ImGui::BeginMenu("File"))
+			{
+				if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
+				if (ImGui::MenuItem("Save", "Ctrl+S")) { /* Do stuff */ }
+				if (ImGui::MenuItem("Close", "Ctrl+W")) { p_open = false; }
+				ImGui::EndMenu();
+			}
+			if (ImGui::BeginMenu("Draw Mode"))
+			{
+				if (ImGui::MenuItem("Draw Torque", nullptr, &bsel_torquedraw))	{ bsel_torquedraw = true;	bsel_powerdraw = false; }
+				if (ImGui::MenuItem("Draw Power", nullptr, &bsel_powerdraw))	{ bsel_torquedraw = false;	bsel_powerdraw = true; }
+				ImGui::EndMenu();
+			}
+			ImGui::EndMenuBar();
+		}
+
+		ImGui::End(); // end window
+
+		window.clear();
+		ImGui::SFML::Render(window);
+		window.display();
+	}
+
+	ImGui::SFML::Shutdown();
+}
---
+++ TorqueDrawer/utils/CurveEval.hpp
@@ -0,0 +1,77 @@
+﻿#pragma once
+
+#include <vector>
+
+class InterpCurveConstant
+{
+public:
+	struct ICPoint
+	{
+		double inV;
+		double outV;
+	};
+
+protected:
+	std::vector< ICPoint > Points;
+
+public:
+
+	//constructor
+	InterpCurveConstant() = default;
+	//InterpCurveConstant( ... ) {}
+
+	//destructor
+	//-------------------------
+
+	size_t	AddPoint(const double inVal, const double outVal);
+	double	Eval(const double InVal, const double& Default) const;
+
+	void print(std::ostream& out);
+	void dump(std::ostream& out);
+	void load(std::ostream& out);
+	void save(std::ostream& out);
+};
+
+
+
+inline size_t	InterpCurveConstant::AddPoint(const double inVal, const double outVal)
+{
+	auto& it = Points.begin();
+	for (; it != Points.end() && it->inV < inVal; ++it);	//find nearest.
+	Points.insert(it, ICPoint{ inVal, outVal });
+	return Points.end() - it;
+}
+
+inline double InterpCurveConstant::Eval(const double InVal, const double& Default) const
+{
+	size_t NumP = Points.size();
+
+	if (NumP == 0) { return Default; }
+	if (NumP < 2 || (InVal <= Points[0].inV)) { return Points[0].outV; }
+	if (InVal >= Points[NumP - 1].inV) { return Points[NumP - 1].outV; }
+
+	//linear search.
+	for (size_t i = 1u; i<NumP; i++)
+	{
+		if (InVal < Points[i].inV)
+		{
+			double dist = Points[i].inV - Points[i - 1].inV;
+
+			if (dist > 0.0)
+			{
+				double alpha = (InVal - Points[i - 1].inV) / dist;
+
+				return (Points[i - 1].outV * alpha) + (Points[i].outV * (1.0 - alpha));
+			}
+			else
+				return Points[i - 1].outV;
+		}
+	}
+	return Points[NumP - 1].outV;
+}
+
+
+inline void InterpCurveConstant::print(std::ostream& out) {}
+inline void InterpCurveConstant::dump(std::ostream& out) {}
+inline void InterpCurveConstant::load(std::ostream& out) {}
+inline void InterpCurveConstant::save(std::ostream& out) {}
